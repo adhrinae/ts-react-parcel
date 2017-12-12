bd71d68c0db8b0cb4ba2f33514f770b3baaeba7b
Ahn Dohyung <emaren84@gmail.com>
2017-12-12 13:36:16 +0900

    Add Switch Component

---
+++ src/Switch.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+
+interface SwitchProps {
+  on: boolean;
+  className: string;
+  onClick(): void;
+}
+
+export default function Switch({ on, className = "", ...props }: SwitchProps) {
+  return (
+    <div className="toggle">
+      <input className="toggle-input" type="checkbox" />
+      <button
+        className={`${className} toggle-btn ${on ? "toggle-btn-on" : "toggle-btn-off"}`}
+        {...props}
+      />
+    </div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import "./index.css";
 
 const App = (): JSX.Element => <div className="container" />;
 
